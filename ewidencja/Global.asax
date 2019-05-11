ac8acac9ee9ec4a909846f5ecb8f3deabeb7ec6a
Kinga Pszeniczna <w57122@student.wsiz.rzeszow.pl>
2019-05-11 19:20:37 +0200

    Dodaj pliki projektów.

---
+++ ewidencja/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ewidencja.MvcApplication" Language="C#" %>
---
+++ ewidencja/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ewidencja.Startup))]
+namespace ewidencja
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
