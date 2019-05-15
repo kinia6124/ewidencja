f8913bb2b1c14b94b88ff896b9beb693d5d3ecac
Kinga Pszeniczna <w57122@student.wsiz.rzeszow.pl>
2019-05-15 23:28:01 +0200

    fix

---
+++ ewidencja/Models/Obywatel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ewidencja.Models
+{
+    public class Obywatel
+    {
+        [Key]
+        public int Idobywatel { get; set; }
+        public char Imie { get; set; }
+        public char Nazwisko { get; set; }
+        public string PESEL { get; set; }
+    }
+}
